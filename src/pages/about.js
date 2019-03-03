4f23f11f860ad8c0dd2dd256e8cd65023e131b21
Michael Abdulai <mabdulai90@gmail.com>
2019-03-03 20:38:13 +0100

    Updates!

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
 import styled from 'styled-components';
+
 const Container = styled.section`
   max-width: 1300px;
   margin: 0 auto;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  width: 65%;
+  padding-right: 120px;
+`;
+const Right = styled.div`
+  width: 35%;
 `;
 
 const Title = styled.h1`
@@ -34,50 +47,83 @@ const BlockTitle = styled.h3`
   font-size: 18px;
   padding-bottom: 4px;
 `;
-const BlockText = styled.div`
-  font-family: 'roboto mono';
-  font-size: 16px;
+const BlockText = styled.p``;
+
+const StatBlock = styled.div`
+  padding-bottom: 24px;
+
+  strong {
+    background: #000;
+    color: #fff;
+    font-weight: 300;
+    padding: 4px 8px;
+  }
 `;
 
+const Stats = styled.p`
+  display: flex;
+  flex-direction: column;
+`;
 class AboutsPage extends Component {
   render() {
     return (
       <Layout>
         <Container>
-          <Title>Michael Abdulai</Title>
-          <Subtitle>Frontend Developer @ ABOSS </Subtitle>
-          <Block>
-            <BlockTitle>What are React Hooks</BlockTitle>
-            <BlockText>
-              Writing React Components has gone through different phases since the beginning. We started out with.
-              CreateReactClass. This was because ES6 was not yet fully available and therefor we couldn't use Classes
-              yet. With the release of React 16 we would have a new way of creating React components. The before
-              mentioned createReactClass was deprecated for the new ES6 syntax of writing classes with React.component
-              Extends ComponentName. This was the way of writing stateful components with React. If you had simple
-              components that did not have any state you could also write Stateless Components. These components did not
-              rely on the Class syntax that would often confuse newcomers. Stateless components are regular Javascript
-              functions this brings in some constraints but also some benefits.
-            </BlockText>
-          </Block>
-          <Block>
-            <BlockText>
-              Writing React Components has gone through different phases since the beginning. We started out with.
-              CreateReactClass. This was because ES6 was not yet fully available and therefor we couldn't use Classes
-              yet. With the release of React 16 we would have a new way of creating React components. The before
-              mentioned createReactClass was deprecated for the new ES6 syntax of writing classes with React.component
-              Extends ComponentName. This was the way of writing stateful components with React. If you had simple
-              components that did not have any state you could also write Stateless Components. These components did not
-              rely on the Class syntax that would often confuse newcomers. Stateless components are regular Javascript
-              functions this brings in some constraints but also some benefits.
-            </BlockText>
-          </Block>
-          <Block>
-            <BlockText>
-              With the release of React 16.8.3 we now have a way to write React components with state without using the
-              Class syntax. There are some new functions added to the React library to achieve this and we will walk
-              through them throughout the rest of this study of React Hooks.
-            </BlockText>
-          </Block>
+          <ContentContainer>
+            <Left>
+              <Title>Michael Abdulai</Title>
+              <Subtitle>Frontend Developer @ ABOSS </Subtitle>
+              <Block>
+                <BlockTitle>What are React Hooks</BlockTitle>
+                <BlockText>
+                  Writing React Components has gone through different phases since the beginning. We started out with.
+                  CreateReactClass. This was because ES6 was not yet fully available and therefor we couldn't use
+                  Classes yet. With the release of React 16 we would have a new way of creating React components. The
+                  before mentioned createReactClass was deprecated for the new ES6 syntax of writing classes with
+                  React.component Extends ComponentName. This was the way of writing stateful components with React. If
+                  you had simple components that did not have any state you could also write Stateless Components. These
+                  components did not rely on the Class syntax that would often confuse newcomers. Stateless components
+                  are regular Javascript functions this brings in some constraints but also some benefits.
+                </BlockText>
+              </Block>
+              <Block>
+                <BlockText>
+                  Writing React Components has gone through different phases since the beginning. We started out with.
+                  CreateReactClass. This was because ES6 was not yet fully available and therefor we couldn't use
+                  Classes yet. With the release of React 16 we would have a new way of creating React components. The
+                  before mentioned createReactClass was deprecated for the new ES6 syntax of writing classes with
+                  React.component Extends ComponentName. This was the way of writing stateful components with React. If
+                  you had simple components that did not have any state you could also write Stateless Components. These
+                  components did not rely on the Class syntax that would often confuse newcomers. Stateless components
+                  are regular Javascript functions this brings in some constraints but also some benefits.
+                </BlockText>
+              </Block>
+              <Block>
+                <BlockText>
+                  With the release of React 16.8.3 we now have a way to write React components with state without using
+                  the Class syntax. There are some new functions added to the React library to achieve this and we will
+                  walk through them throughout the rest of this study of React Hooks.
+                </BlockText>
+              </Block>
+            </Left>
+            <Right>
+              <Stats>
+                Stats
+                <StatBlock>
+                  <strong>INTERESTS</strong> reading, travel, finance, cars, running, photography, design, coffee,
+                  sushi, modern architecture, prototyping, a clean workspace, cooking
+                </StatBlock>
+                <StatBlock>
+                  <strong>INTERESTS</strong> reading, travel, finance, cars, running, photography, design, coffee,
+                  sushi, modern architecture, prototyping, a clean workspace, cooking
+                </StatBlock>
+                <StatBlock>
+                  <strong>INTERESTS</strong> reading, travel, finance, cars, running, photography, design, coffee,
+                  sushi, modern architecture, prototyping, a clean workspace, cooking
+                </StatBlock>
+              </Stats>
+            </Right>
+          </ContentContainer>
         </Container>
       </Layout>
     );
--- src/pages/travel/new-york.js
+++ src/pages/travel/new-york.js
@@ -87,13 +87,14 @@ const Block = styled.p`
 
 const BlockTitle = styled.h3`
   font-family: 'Gilroy';
-  font-size: 18px;
+  font-size: 32px;
   padding-bottom: 4px;
+  text-transform: uppercase;
 `;
 const BlockText = styled.div`
-  font-family: 'roboto mono';
-  font-size: 16px;
-  max-width: 100ch;
+  font-size: 18px;
+  line-height: 26px;
+  max-width: 60ch;
 `;
 
 const Gallery = styled(ImageGallery)`
@@ -114,13 +115,10 @@ class NewYorkPage extends Component {
           <Block>
             <BlockTitle>What did we do yo!</BlockTitle>
             <BlockText>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae veniam quidem ab, harum provident
-              ut quam mollitia fugiat consequatur quos nostrum magni expedita molestiae veritatis aspernatur! Eveniet,
-              ut est reprehenderit aliquam quis necessitatibus tempore nisi illum in quam omnis voluptas obcaecati,
-              eligendi non sapiente pariatur. Libero tempore sequi ipsam eaque placeat illum ipsa nulla eius voluptatem
-              vero, nobis doloribus velit est beatae esse quis suscipit, ut impedit! Corrupti, ullam. Quae asperiores
-              quos architecto, accusantium iste similique eius dignissimos perferendis debitis deleniti porro iure ut
-              eum aspernatur. In odio assumenda repudiandae nam illo sit quo blanditiis, minima ducimus, ab odit!
+              As businesses prioritize employee experience and consider what it means to have a distributed workforce,
+              the real estate industry watches outsiders, such as WeWork and Convene, create new value and markets. With
+              the future of work in mind, EQ (formerly Equity Office Properties) is proactively reimagining how
+              businesses experience space as a service.
             </BlockText>
           </Block>
           <Spacer />
