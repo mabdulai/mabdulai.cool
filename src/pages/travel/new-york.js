823eb4ad5aaf3a910758cff6dabe3314b42e7cb1
Michael-ABOSS <michael@a-boss.net>
2019-05-02 15:39:40 +0200

    Update new-york.js

--- src/pages/travel/new-york.js
+++ src/pages/travel/new-york.js
@@ -72,10 +72,10 @@ class NewYorkPage extends Component {
           <Block>
             <BlockText>
               The Big Apple. After a pretty relaxing flight filled with sleep and onboard movies we arrived at JFK
-              Airport. We stayed at the Wellington Hotel on 7th avenue close to Times Squares. It felt like a perfect
-              location for a Tourist. Close to all the action and everything we wanted to see easily reachable. We
-              visited Times Square multiple times just because it was so close to our Hotel and we liked the atmosphere
-              that it had. We also had plans to visit a broadway show and that was just off Times Square.
+              Airport. We stayed at the Wellington Hotel on 7th avenue close to Times Square. It felt like a perfect
+              location for a tourist. Close to all the action and everything we wanted to see easily reachable. We
+              visited Times Square multiple times just because it was so close to our hotel and we liked the atmosphere
+              that it had. We also had plans to visit a Broadway show and that was just off Times Square.
             </BlockText>
           </Block>
           <Spacer />
@@ -89,9 +89,9 @@ class NewYorkPage extends Component {
               rest of the city. New York really feels like a city that never sleeps. With constant reminders of the
               large population filling the streets, subways and buildings. You can't go for five minutes without at
               least hearing a sirene, or the beeps of a cop car. But walking towards Central Park you already notice a
-              big difference. Where normally the view of the sky is blocked by the immensly tall buildings of New York
-              you suddenly start to see more and more blue of sky. You are greeted at the beginning of Central Park with
-              the many horse carriages wanting to take you on a stroll through the park. The city was in full
+              big difference. Where normally the view of the sky is blocked by the immensely tall buildings of New York
+              you suddenly start to see more and more the blue of the sky. You are greeted at the beginning of Central Park with
+              the many horse carriages wanting to take you on a ride through the park. The city was in full
               preperations for the New York marathon at the time when we arrived. This was amazing because of all of the
               setup for the event around Central Park. We actually walked across the finish line of the marathon. From
               the park we made a quick detour to visit the Metropolitan Museum (The Met). Such a beautiful building.
@@ -106,8 +106,8 @@ class NewYorkPage extends Component {
             <BlockTitle>So much food</BlockTitle>
             <BlockText>
               Our days were filled with walking around the city. Planning out a route and things we wanted to see and
-              foods we wanted to eat in the morning. And relaxing and eating some more food in the evenings. The city is
-              just brimming with those classic american foods that every tourists wants to eat. From New York Pizza,
+              foods we wanted to eat in the morning and relaxing and eating some more food in the evenings. The city is
+              just brimming with those classic american foods that every tourist wants to eat. From New York Pizza,
               donuts, hot dogs and all kinds of other tasty fast food to some more of the sweeter foods America has to
               offer. We needed to test out all of the candies we can not get in Holland and see if they live up to their
               reputations (Spoilers: they did).
@@ -118,13 +118,13 @@ class NewYorkPage extends Component {
           <Block>
             <BlockTitle>Being the perfect tourist</BlockTitle>
             <BlockText>
-              There are ofcourse certain spots you have to visit when you are in New York. We were there for a limited
+              There are of course certain spots you have to visit when you are in New York. We were there for a limited
               time so we had to make some choices of the places we really wanted to see and some things that we had or
               wanted to skip. After reading a lot of mediocre reviews we decided to skip the Statue of liberty. We did
               get a good view of it when we crossed the Brooklyn Bridge by foot. In the end we felt like we made a good
               choice. Some of the highlights of things we visited and saw were. Rockafeller building, Little Italy,
-              China Town, Broadway, Brooklyn. But one of the most amazing things we did was being in America for
-              Halloween 👻. We went to the parade down town and it was just a spectical. It was just like in the movies,
+              China Town, Broadway and Brooklyn. But one of the most amazing things we did was being in America for
+              Halloween 👻. We went to the parade downtown and it was just a spectical. It was just like in the movies,
               big, well organised and just all around American Halloween. We had an amazing time!
             </BlockText>
           </Block>
