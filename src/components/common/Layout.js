100af3b6ab507ed41542bffd248a79c283309074
Michael Abdulai <michael@a-boss.net>
2020-06-25 18:27:13 +0200

    RESET!

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -3,24 +3,14 @@ import styled, { ThemeProvider } from "styled-components";
 import Helmet from "react-helmet";
 import theme from "../../style/theme";
 import GlobalStyle from "../../style/global";
-import Nav from "./Nav";
-import Header from "./Header";
-import Footer from "./Footer";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 `;
-
-const Main = styled.main`
-  background: ${(props) => props.theme.main};
-  margin: 0 auto;
-  width: 1440px;
-`;
 class Layout extends Component {
   render() {
-    const { displayHeader = true } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Helmet>
@@ -31,10 +21,7 @@ class Layout extends Component {
         </Helmet>
         <Container>
           <GlobalStyle whiteColor />
-          <Nav>Nav</Nav>
-          {displayHeader && <Header>Header</Header>}
-          <Main>{this.props.children}</Main>
-          <Footer>Footer</Footer>
+          {this.props.children}
         </Container>
       </ThemeProvider>
     );
